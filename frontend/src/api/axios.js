a121ced981f503c7d1cd657c6166fa7f2a552307
EmmanuelMbamba <emmanuelmbamba87@gmail.com>
2026-08-02 16:46:26 +0100

    Fix axios JWT interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,7 +7,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  console.log("TOKEN ENVOYÉ :", token ? "Présent" : "Absent");
+  console.log("TOKEN :", token);
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
