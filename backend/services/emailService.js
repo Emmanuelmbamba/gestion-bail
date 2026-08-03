37477b4e2ddf0a8017d795fc8c5522a3d8919fd3
EmmanuelMbamba <emmanuelmbamba87@gmail.com>
2026-08-03 02:48:17 +0100

    Fix Brevo email service

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,4 +1,4 @@
-const brevo = require("@getbrevo/brevo");
+/* const brevo = require("@getbrevo/brevo");
 
 const envoyerEmail = async (email, sujet, message) => {
   try {
@@ -35,5 +35,53 @@ const envoyerEmail = async (email, sujet, message) => {
     throw error;
   }
 };
+
+module.exports = envoyerEmail;*/
+
+const brevo = require("@getbrevo/brevo");
+
+const envoyerEmail = async (email, sujet, message) => {
+  try {
+
+    const apiInstance = new brevo.TransactionalEmailsApi();
+
+    apiInstance.setApiKey(
+      brevo.TransactionalEmailsApiApiKeys.apiKey,
+      process.env.BREVO_API_KEY
+    );
+
+    const sendSmtpEmail = new brevo.SendSmtpEmail();
+
+    sendSmtpEmail.subject = sujet;
+
+    sendSmtpEmail.textContent = message;
+
+    sendSmtpEmail.sender = {
+      name: "Gestion-Bail RDC",
+      email: process.env.EMAIL_FROM
+    };
+
+    sendSmtpEmail.to = [
+      {
+        email: email
+      }
+    ];
+
+
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+    console.log("✅ Email envoyé :", email);
+
+  } catch (error) {
+
+    console.error(
+      "❌ Erreur Brevo :",
+      error
+    );
+
+    throw error;
+  }
+};
+
 
 module.exports = envoyerEmail;
