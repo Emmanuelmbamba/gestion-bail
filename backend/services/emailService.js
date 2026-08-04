f0b34f8c09de50bc5cf4a0592b72c82506998e9e
EmmanuelMbamba <emmanuelmbamba87@gmail.com>
2026-08-04 15:16:36 +0100

    Correction import Brevo API

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,65 +1,32 @@
-/* const brevo = require("@getbrevo/brevo");
+const brevo = require("@getbrevo/brevo").default;
+
 
 const envoyerEmail = async (email, sujet, message) => {
   try {
 
     const apiInstance = new brevo.TransactionalEmailsApi();
 
-    apiInstance.setApiKey(
-      brevo.TransactionalEmailsApiApiKeys.apiKey,
-      process.env.BREVO_API_KEY
-    );
-
-    const mail = new brevo.SendSmtpEmail();
-
-    mail.subject = sujet;
-    mail.textContent = message;
-
-    mail.sender = {
-      name: "Gestion-Bail",
-      email: process.env.EMAIL_FROM,
-    };
-
-    mail.to = [
-      {
-        email: email,
-      },
-    ];
-
-    await apiInstance.sendTransacEmail(mail);
-
-    console.log("✅ Email envoyé :", email);
-
-  } catch (error) {
-    console.error("❌ Erreur Brevo :", error);
-    throw error;
-  }
-};
-
-module.exports = envoyerEmail;*/
-
-const brevo = require("@getbrevo/brevo");
-
-const envoyerEmail = async (email, sujet, message) => {
-  try {
-
-    const apiInstance = new brevo.TransactionalEmailsApi();
 
     apiInstance.setApiKey(
       brevo.TransactionalEmailsApiApiKeys.apiKey,
       process.env.BREVO_API_KEY
     );
+
 
     const sendSmtpEmail = new brevo.SendSmtpEmail();
 
+
     sendSmtpEmail.subject = sujet;
 
+
     sendSmtpEmail.textContent = message;
+
 
     sendSmtpEmail.sender = {
       name: "Gestion-Bail RDC",
       email: process.env.EMAIL_FROM
     };
+
 
     sendSmtpEmail.to = [
       {
@@ -68,11 +35,18 @@ const envoyerEmail = async (email, sujet, message) => {
     ];
 
 
-    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    const response = await apiInstance.sendTransacEmail(
+      sendSmtpEmail
+    );
+
 
     console.log("✅ Email envoyé :", email);
 
-  } catch (error) {
+
+    return response;
+
+
+  } catch(error) {
 
     console.error(
       "❌ Erreur Brevo :",
@@ -80,6 +54,7 @@ const envoyerEmail = async (email, sujet, message) => {
     );
 
     throw error;
+
   }
 };
 
